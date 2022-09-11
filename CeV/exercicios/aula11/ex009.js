a22cdea2b392b5b3550afe039011edc8cec54d37
Rafaelottalves <rafaelott12345@gmail.com>
2022-09-10 22:15:45 -0300

    novos

---
+++ CeV/exercicios/aula11/ex009.js
@@ -0,0 +1,11 @@
+var pais = 'França'
+
+console.log(`Vivendo em ${pais}`)
+
+if (pais == 'Brasil') { // Condição Composta
+    console.log('Você é Brasileiro')
+}
+
+else {
+    console.log('Você é Estrangeiro')
+}
---
+++ CeV/praticas/p004/p004.js
@@ -0,0 +1,13 @@
+var nota = 10
+
+if (nota > 6) {
+    console.log('Você esta APROVADO!')
+}
+
+else if (nota == 6) {
+    console.log('Você esta em RECUPERAÇÃO!')
+}
+
+else {
+    console.log('Você esta REPROVADO!')
+}
