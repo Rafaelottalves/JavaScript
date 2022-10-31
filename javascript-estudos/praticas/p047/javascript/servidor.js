99498517d7e828743d6a46c56972c5bd72c7cff6
Rafaelottalves <rafaelott12345@gmail.com>
2022-10-31 17:14:43 -0300

    novos

---
+++ javascript-estudos/praticas/p047/javascript/servidor.js
@@ -0,0 +1,4 @@
+const nome = document.getElementById('entrada-nome').value
+const email = document.getElementById('entrada-email').value
+const senha = document.getElementById('entrada-senha').value
+
