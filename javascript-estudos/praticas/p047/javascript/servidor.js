62af913250d0ad092a3d300d73a2f35267be5dda
Rafaelottalves <rafaelott12345@gmail.com>
2022-11-01 06:55:31 -0300

    novos

--- javascript-estudos/praticas/p047/javascript/servidor.js
+++ javascript-estudos/praticas/p047/javascript/servidor.js
@@ -1,4 +1,27 @@
-const nome = document.getElementById('entrada-nome').value
-const email = document.getElementById('entrada-email').value
-const senha = document.getElementById('entrada-senha').value
+let dadosCadastro = []
 
+function pegaDados() {
+    let nomeCadastro = document.getElementById('entrada-nome').value
+    let emailCadastro = document.getElementById('entrada-email').value 
+    let senhaCadastro = document.getElementById('entrada-senha').value
+    
+    let dados = {
+        nome: nomeCadastro,
+        email: emailCadastro,
+        senha: senhaCadastro
+    }
+
+    dadosCadastro.push(dados)
+
+    let form = document.querySelector('.formulario')
+    form.reset()
+
+    window.location.href = '../pag-inicio/index.html'
+}
+
+function validação() {
+    let emailLogin = document.getElementById('entrada-email-login')
+    let senhaLogin = document.getElementById('entrada-senha-login')
+
+    console.log(emailLogin)
+}
