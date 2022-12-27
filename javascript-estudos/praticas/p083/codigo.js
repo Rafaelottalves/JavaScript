6e46e40410b9d73a4ffd1a5c812d1d060a8a66b5
Rafaelottalves <rafaelott12345@gmail.com>
2022-12-27 19:45:06 -0300

    novos

---
+++ javascript-estudos/praticas/p083/codigo.js
@@ -0,0 +1,10 @@
+let obj = {
+    nome: 'rafael', 
+    idade: 18,
+    animais: ['cachorro', 'gato', 'galinha'],
+    celular: 1,
+    comidasPreferidas: {caféDaManha: ['café', 'pão'], almoço: ['arroz', 'feijão', 'galinha'], jantar: 'feijoada'},
+    outros: f
+}
+
+console.log(obj.propertyIsEnumerable('outros'))
