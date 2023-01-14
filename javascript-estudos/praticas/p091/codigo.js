578b33501b212727bb23cc766288d5d039f32acd
Rafaelottalves <rafaelott12345@gmail.com>
2023-01-14 19:23:42 -0300

    novos

--- javascript-estudos/praticas/p091/codigo.js
+++ javascript-estudos/praticas/p091/codigo.js
@@ -1,3 +1,1 @@
-// AGORA: atualizar "os comandos gerais"
-//depois ir praticando na sequencia dos doc (daquele jeito que a gente sabe), depois fazer os projetos de todos os cursos (e talvez outros que não estejam)
-
+//AGORA: fazer os projetos de todos os cursos (e talvez outros que não estejam)
