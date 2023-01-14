e6e6f801a47af06a8d2e0440c61a62afa18200b5
Rafaelottalves <rafaelott12345@gmail.com>
2023-01-13 23:18:55 -0300

    novos

--- javascript-estudos/praticas/p091/codigo.js
+++ javascript-estudos/praticas/p091/codigo.js
@@ -1,3 +1,3 @@
-// Agora ver o ultimo curso: se ve que ja viu nem assiste
-// depois que terminar os cursos: atualizar "os comandos gerais", depois ir praticando na sequencia dos doc (daquele jeito que a gente sabe), depois fazer os projetos
+// AGORA: atualizar "os comandos gerais"
+//depois ir praticando na sequencia dos doc (daquele jeito que a gente sabe), depois fazer os projetos de todos os cursos (e talvez outros que não estejam)
 
