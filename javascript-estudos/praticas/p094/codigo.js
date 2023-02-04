403a1531172141cc81e224307d3696cbed311f34
Rafaelottalves <rafaelott12345@gmail.com>
2023-02-04 11:02:36 -0300

    novos

--- javascript-estudos/praticas/p094/codigo.js
+++ javascript-estudos/praticas/p094/codigo.js
@@ -1,6 +1,5 @@
 // agora manipulação de formularios - depois manipulação de video - audio ???
 // em uma verificação for, para não olhar o resto digite: return (dentro da verificacao)
 // paradigmas do javascript - transições js
-
-
+// preventDefault() - geralmente é feito nos formularios
 
