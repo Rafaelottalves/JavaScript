1247fb4f5cd56f1f571b5cd405b1ab2b5ea1c530
Rafael Ott Alves <105594751+Rafaelottalves@users.noreply.github.com>
2023-02-23 02:14:37 -0300

    notas

--- javascript-estudos/praticas/p094/codigo.js
+++ javascript-estudos/praticas/p094/codigo.js
@@ -1,4 +1,3 @@
-// agora manipulação de formularios - depois manipulação de video - audio ???
 // em uma verificação for, para não olhar o resto digite: return (dentro da verificacao)
 // paradigmas do javascript - transições js
 // preventDefault() - geralmente é feito nos formularios
@@ -6,3 +5,14 @@
 // COMO DOCUMENTar um projeto - como fazer uma documentação
 // em volta
 //4 - proprio elemento -> atributos -> conteudo -> em volta (eventListener, removeEventListener...)
+
+/* let div = document.getElementById('bloco')
+
+div.addEventListener('') */
+
+/* let input = document.querySelector('input')
+input.addEventListener('blur', sai) */
+
+function sai() {
+    alert('saiuuu')
+}
