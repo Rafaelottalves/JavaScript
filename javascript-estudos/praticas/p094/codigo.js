fa0006d177a7416d1462121afdf349c999fb682d
Rafael Ott Alves <105594751+Rafaelottalves@users.noreply.github.com>
2023-02-25 12:41:11 -0300

    novos

--- javascript-estudos/praticas/p094/codigo.js
+++ javascript-estudos/praticas/p094/codigo.js
@@ -5,5 +5,6 @@
 // COMO DOCUMENTar um projeto - como fazer uma documentação
 // em volta
 //4 - proprio elemento -> atributos -> conteudo -> em volta (eventListener, removeEventListener...)
-// [AGORA] praticar eventos!!
-
+// ANTES DE IR PARA CONCEITOS, PRATICAR BASTANTE O JAVASCRIPT
+//[DEPOIS] projeto utilizando eventos, um site que sobre algo que possa colocar bastante eventos!!
+// depois um site completo pra vender em algum site (experiencia)!!
