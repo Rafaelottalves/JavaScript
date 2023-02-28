9a19ba5cacd82c56ac92ebc0b24e78c0c1648c9d
Rafael Ott Alves <105594751+Rafaelottalves@users.noreply.github.com>
2023-02-28 18:35:03 -0300

    novos

---
+++ javascript-estudos/praticas/p094/codigo.js
@@ -0,0 +1,24 @@
+//dps de animações fazer transições
+
+/* let div = document.getElementById('div')
+div.animate
+div.getAnimations
+div.onanimationcancel
+div.onanimationend
+div.onanimationiteration
+div.onanimationstart
+div.style.animation
+div.style.animationDelay
+div.style.animationDirection
+div.style.animationDuration
+div.style.animationFillMode
+div.style.animationIterationCount
+div.style.animationName
+div.style.animationPlayState
+div.style.animationTimingFunction
+requestAnimationFrame
+cancelAnimationFrame
+setTimeout
+clearTimeout
+setInterval
+clearInterval */
