8127d7c45b667ddc685fe45a2b8588c1a4191462
Rafael Ott Alves <105594751+Rafaelottalves@users.noreply.github.com>
2023-03-18 19:11:56 -0300

    novos

--- javascript-estudos/praticas/p096/codigo.js
+++
@@ -1,2 +0,0 @@
-/* Começar falando sobre acessibilidade → falar sobre acessibilidade no js → comandos/padrões/boas praticas… de acessibilidade no js (o que usar para acessibilidade no js) */
-/* falta(terminar style?, eventos! e DOM!) */
