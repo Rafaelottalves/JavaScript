76d8648ed7dc1c23ea26daac7ca5256e211e809b
Rafael Ott Alves <105594751+Rafaelottalves@users.noreply.github.com>
2023-04-04 13:37:24 -0300

    novos

--- javascript-estudos/praticas/p097 - acess/codigo.js
+++ javascript-estudos/praticas/p097 - acess/codigo.js
@@ -1,21 +1,13 @@
-/* Começar falando sobre acessibilidade → falar sobre acessibilidade no js → comandos/padrões/boas praticas… de acessibilidade no js (o que usar para acessibilidade no js) -> reunir imagens */
 /* falta(terminar style?, eventos! e DOM!) */
-/* acss 04 - guia de acessibilidade js, o que fazer, situações, o que fazer em situações, o que pode fazer pra ter acss em js? quais ações tomar em situações? o que prevenir */
-/* OVERVIEW mostrando todos os possiveis alternativas para acessibilidade ou mostrando todos os problemas */
 /* QUANDO TUDO DE JS ACABAR PRATICAR BASTANTE ANTES DE SAIR, DAR UMA ATENÇÃO BOA PRA PRATICAR TUDO O QUE FOI ESTUDADO, TANTO INDIVIDUAL COMO JUNTO */
 
-/* sem visão, sem audição, sem mouse, sem teclado */
-/* sem visão (LEITOR DE TELA) */
-
-/* tipos de erros */
 /* ACESSIBILIDADE => ERROS => PRATICAR JS => CONCEITOS => -TERMINOU ESPECIFICOS- */
 /* pasta -> aleatorios - soltos? */
 /* arrow function perde a referencia ao elemento this */
 
 /* objeto arguments - https://www.youtube.com/watch?v=w03VD6yOh6U&ab_channel=BrazilianDev */
 
-/* É possível gerar vários tipos de erros especificos alem do generico (new Error()), cada caso tem seu erro pré-definido, basta saber qual
+/* erros promises e calbacks, erros customizados
+voltar a web */
+/* PRATICAR BASTANTE ERROS AGORA!! */
 
-Error() é um erro generico */
-
-/* arrumas o arquivo sobre erros e exceções (n são a mesma coisa!) -Uma exceção é um objeto de erro que foi lançado.- */
