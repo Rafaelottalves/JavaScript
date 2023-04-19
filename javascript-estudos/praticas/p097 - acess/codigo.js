a9278974020a09aa8524fae223c54a0d8b612122
Rafael Ott Alves <105594751+Rafaelottalves@users.noreply.github.com>
2023-04-19 16:51:30 -0300

    novos

--- javascript-estudos/praticas/p097 - acess/codigo.js
+++ javascript-estudos/praticas/p097 - acess/codigo.js
@@ -22,10 +22,17 @@
 /* event loop */
 /* biblioteca request ja ta defasada e antiga */
 /* o axios n trabalha com callback mais porque ja ta ficando ultrapassado */
-/* Promise + loop */
 
-/* API - fetch etc.. */
 /* especificos - depurar, event loop ? */
+
+/* fazer uma extensão em javscript pra rolagem automatica de mangas */
+
+/* FAZER O HTTP DEPOIS A GENTE VE ONDE ENCAIXA ELE, MAS N É EM PROMISE, NA PROMISE VAMOS DEIXAR O FETCH, ENDPOINT-GET-POST, DEPOIS TIRA O HTTP DE LA E VE ONDE VAI, É UM TÓPICO EMPORTANTE ENT CUIDADO, SE N SE ENCAIXAR COM NADA DEIXAR LA MESMO
+- O HTTP MERECE UMA PASTA EM ESPECIFICOS, DEIXAR PRA LA POR AGORA, VAMOR FAZER SÓ O GETCH-ENDPOINT-GET-POST */
+
+/* depois que terminar, ver a organização do javascript, ver o que deixamos o que podemos adicionar, realocar, reestudar (como localstorage etc), trocar etc. etc. (PARECE QUE TA TUDO BAGUNÇADO) */
+
+/* quando voltar pra html-css dar um foco pro touch, e aparelhos moveis em si */
 
 /* -------------------- */
 
