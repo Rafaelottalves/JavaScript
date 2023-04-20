bf6ebe08a282983eeaed8baa1fe5a36b62f287b8
Rafael Ott Alves <105594751+Rafaelottalves@users.noreply.github.com>
2023-04-20 19:08:09 -0300

    novos

--- javascript-estudos/praticas/p097 - acess/codigo.js
+++ javascript-estudos/praticas/p097 - acess/codigo.js
@@ -1,17 +1,11 @@
 /* falta(terminar style?, eventos! e DOM!) */
 /* QUANDO TUDO DE JS ACABAR PRATICAR BASTANTE ANTES DE SAIR, DAR UMA ATENÇÃO BOA PRA PRATICAR TUDO O QUE FOI ESTUDADO, TANTO INDIVIDUAL COMO JUNTO */
 
-/* ACESSIBILIDADE => ERROS => PRATICAR JS => CONCEITOS => -TERMINOU ESPECIFICOS- */
-/* pasta -> aleatorios - soltos? */
-/* arrow function perde a referencia ao elemento this */
-
 /* objeto arguments - https://www.youtube.com/watch?v=w03VD6yOh6U&ab_channel=BrazilianDev */
-
 
 /*voltar a web */
 
 /* destructuring */
-/* require() - get() */ 
 
 /* axios - biblioteca pra consumir API */
 
@@ -19,11 +13,8 @@
 
 /* sketch book digital - site/app?? */
 
-/* event loop */
 /* biblioteca request ja ta defasada e antiga */
 /* o axios n trabalha com callback mais porque ja ta ficando ultrapassado */
-
-/* especificos - depurar, event loop ? */
 
 /* fazer uma extensão em javscript pra rolagem automatica de mangas */
 
@@ -34,5 +25,10 @@
 
 /* quando voltar pra html-css dar um foco pro touch, e aparelhos moveis em si */
 
+/* mostrar o processo da criação pro cliente, cada passo que será tomado para a construção do site */
+
+/* arrow function perde a referencia ao elemento this */
+
 /* -------------------- */
 
+/* event loop - depuração - soltos - http */
